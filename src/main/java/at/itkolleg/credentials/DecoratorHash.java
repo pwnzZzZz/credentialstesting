96d0e9817b24b845dcf00f3c80c1b75bf20134ca
pwnz <marceschranz@tsn.at>
2022-11-16 08:54:53 +0100

    DecoratorHash Implementierung

--- src/main/java/at/itkolleg/credentials/DecoratorHash.java
+++ src/main/java/at/itkolleg/credentials/DecoratorHash.java
@@ -1,18 +1,39 @@
 package at.itkolleg.credentials;
 
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 
-
+/**
+ * Hier werden die Zusatzfunktionen für
+ * den Decorator eingebaut
+ */
 public class DecoratorHash extends Decorator{
 
 
+    /**
+     * Übergibt der Mutterklasse Decorator den
+     * benötigten CsvExporter
+     * @param csvExporter Exporter-Objekt das zu verschönern ist
+     */
     public DecoratorHash(CsvExporter csvExporter) {
         super(csvExporter);
     }
 
+
+    /**
+     * Beim export wird die CredentialsList an
+     * die Mutterklasse übergeben
+     * @param credentialsList Login Liste
+     */
     @Override
     public void export(List<Credentials> credentialsList) {
+        String sha256hex = Hashing.sha256()
+                .hashString(credentialsList, StandardCharsets.UTF_8)
+                .toString();
+
         super.export(credentialsList);
     }
 }
